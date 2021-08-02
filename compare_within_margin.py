c561b7d8f6c83d7596c08d74a44f9424cd4d6d01
Anton <orlovantn@yandex.ru>
2021-08-02 15:37:33 +0300

    final version

--- compare_within_margin.py
+++ compare_within_margin.py
@@ -1,9 +1,11 @@
+# https://www.codewars.com/kata/56453a12fcee9a6c4700009c
+
 def close_compare(a, b, margin=0):
-    if a > b and not margin >= a - b:
+    if a > b and not margin >= abs(a - b): # a is higher than b
         return 1
-    elif a < b and not margin >= a - b:
+    elif a < b and not margin >= abs(a - b): # a lower than b
         return -1
-    else:
+    else: # a is close to b
         return 0
 
 var_1 = close_compare(10, 4, 1)
