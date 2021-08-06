b2d5e676e35e023ed3d957bebf0ebe8499ebd06c
Anton <orlovantn@yandex.ru>
2021-08-06 22:51:49 +0300

    opposite number (8 kyu)

---
+++ opposite_number.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+def opposite(number):
+        return number * -1
+
+print(opposite(1))
+print(opposite(-2))
+print(opposite(-1.578))
+print(opposite(0))
