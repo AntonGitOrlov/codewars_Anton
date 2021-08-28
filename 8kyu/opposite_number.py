c9ba3297574b7115fe2b1c2f501e736df650e8c4
Anton <orlovantn@yandex.ru>
2021-08-28 12:05:06 +0300

    Deleted files manually

--- 8kyu/opposite_number.py
+++
@@ -1,9 +0,0 @@
-# https://www.codewars.com/kata/56dec885c54a926dcd001095
-
-def opposite(number):
-        return number * -1
-
-print(opposite(1))
-print(opposite(-2))
-print(opposite(-1.578))
-print(opposite(0))
